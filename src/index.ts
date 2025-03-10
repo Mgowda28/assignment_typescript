85dd3417a108b008bf0738490ae3bc15e0ed469e
Mgowda28 <muddaveregowda.1si21is028@gmail.com>
2025-03-10 16:48:15 +0530

    Created Examples and tested the code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { ReminderDatabase } from "./ReminderDatabase";
 
 
-const db=new ReminderDatabase();
+const db = new ReminderDatabase();
 
 db.createReminder("1","Read a Book","Wanted to read a mythology book","11-03-2025");
-db.updateReminder("2","Buy milk","Buy milk at 1 pm","11-03-2025");
-console.log("Does the reminder with id 1 exixts?",db.exists("1"))
+db.createReminder("2","Buy milk","Buy milk at 1 pm","11-03-2025");
+console.log("Does the reminder with id 1 exists?",db.exists("1"))
 
-db.getAllReminders();
-db.getReminder("1");
-db.removeReminder("1");
-db.getAllReminders();
+console.log(db.getAllReminders());
+console.log(db.getReminder("1"));
+console.log(db.removeReminder("1"));
+console.log(db.getAllReminders());
 db.updateReminder("2","Buy milk","Buy milk at 3 pm","11-03-2025");

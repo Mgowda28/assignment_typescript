360dd8c43ebaa73c5e55490944e16b1165e9d3bb
Mgowda28 <muddaveregowda.1si21is028@gmail.com>
2025-03-10 16:37:48 +0530

    Created Reminder Class

---
+++ src/Reminder.ts
@@ -0,0 +1,29 @@
+type ReminderData = {
+    id: string;
+    title: string;
+    description: string;
+    dueDate: string;
+};
+
+export class Reminder {
+    id: string;
+    title: string;
+    description: string;
+    dueDate: string;
+
+    constructor(id: string, title: string, description: string, dueDate: string) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    toObject(): ReminderData {
+        return {
+            id: this.id,
+            title: this.title,
+            description: this.description,
+            dueDate: this.dueDate,
+        };
+    }
+}
